4c6394964aff11da5fe9cbcdd9b13d74b73d3c43
Daniel <dkweonlee@gmail.com>
2017-07-24 20:26:07 -0400

    export zipcodes from csv

--- slcsp/slcsp.rb
+++ slcsp/slcsp.rb
@@ -3,7 +3,12 @@ require 'pry'
 
 puts 'Please enter data name'
 filename = gets.chomp
-
 zipcode = CSV.read("#{filename}.csv")
+i = 1
+list_zip = []
+while i < zipcode.length
+  list_zip << zipcode[i][0]
+  i+=1
+end
 
 binding.pry
