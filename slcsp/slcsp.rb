f3e62896dfb59a686576d8a26ed65f6f8b3b564b
Daniel <dkweonlee@gmail.com>
2017-09-27 19:14:45 -0400

    searching for rate_area

--- slcsp/slcsp.rb
+++ slcsp/slcsp.rb
@@ -1,14 +1,34 @@
 require 'csv'
 require 'pry'
+class Slcsp_finder
+  slcsp = CSV.read("slcsp.csv")
+  zips = CSV.read("zips.csv")
+  plans = CSV.read("plans.csv")
+  
+  # def list_zipcodes
+  i = 1
+  j = 0
+  # list_zip = []
+  while i < slcsp.length
+   slcsp_zip = slcsp[i][0]
+   while j < zips.length
+    if slcsp_zip == zips[j][0]
+      rate_area = zips[j][4]
+    end 
+    j+=1
+   end
+    i+=1
+  end
+  # end
+  
+  zips = CSV.read("zips.csv").length
+  binding.pry
+  j = 0
+  while i < CSV.read("zips.csv").length
+    
+  end
 
-puts 'Please enter data name'
-filename = gets.chomp
-zipcode = CSV.read("#{filename}.csv")
-i = 1
-list_zip = []
-while i < zipcode.length
-  list_zip << zipcode[i][0]
-  i+=1
 end
+
 
 binding.pry
