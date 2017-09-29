fb65f86f61aa45f501a5c3fe6c286cbcb8cd6da9
Daniel <dkweonlee@gmail.com>
2017-09-28 21:33:39 -0400

    find silver with rate area

--- slcsp/slcsp.rb
+++ slcsp/slcsp.rb
@@ -7,13 +7,19 @@ class Slcsp_finder
   
   # def list_zipcodes
   i = 1
-  j = 0
+  j = 1
+  k = 1
   # list_zip = []
   while i < slcsp.length
    slcsp_zip = slcsp[i][0]
    while j < zips.length
     if slcsp_zip == zips[j][0]
       rate_area = zips[j][4]
+      while k < plans.length
+        if rate_area == plans[k][5] && plans[k][3] == "Silver" 
+        end
+        k += 1
+      end
     end 
     j+=1
    end
