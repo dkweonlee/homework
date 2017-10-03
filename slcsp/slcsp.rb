28e5d1b1cc7a129d7cd1f37780df9fc8917421d3
Daniel <dkweonlee@gmail.com>
2017-10-03 16:32:30 -0400

    found slcsp but need to seperate the loops

--- slcsp/slcsp.rb
+++ slcsp/slcsp.rb
@@ -4,37 +4,35 @@ class Slcsp_finder
   slcsp = CSV.read("slcsp.csv")
   zips = CSV.read("zips.csv")
   plans = CSV.read("plans.csv")
-  
-  # def list_zipcodes
+  plan_compare = []
   i = 1
   j = 1
   k = 1
-  # list_zip = []
   while i < slcsp.length
-   slcsp_zip = slcsp[i][0]
-   while j < zips.length
+  slcsp_zip = slcsp[i][0]
+  while j < zips.length
     if slcsp_zip == zips[j][0]
       rate_area = zips[j][4]
       while k < plans.length
-        if rate_area == plans[k][5] && plans[k][3] == "Silver" 
-        end
-        k += 1
+        if rate_area == plans[k][5] && plans[k][3] == "Silver"
+          if  plan_compare.length < 2
+            plan_compare<<plans[k][4]
+            plan_compare.sort!
+          end
+          if plans[k][4] < plan_compare[1]
+             plan_compare.pop
+             plan_compare<<plans[k][4]
+             plan_compare.sort!
+          end
+        end 
+      end
+      k += 1
       end
     end 
     j+=1
-   end
-    i+=1
+    end
+  i+=1
   end
-  # end
-  
-  zips = CSV.read("zips.csv").length
-  binding.pry
-  j = 0
-  while i < CSV.read("zips.csv").length
-    
-  end
-
-end
 
 
 binding.pry
